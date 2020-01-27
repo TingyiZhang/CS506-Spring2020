644b8c88f8ed20d80feb7f563442e543ea41ed6e
TingyiZhang <tingyizhang97@gmail.com>
2020-01-27 17:42:13 -0500

    Modied my lake

--- 01-python/outdoors/lake.py
+++ 01-python/outdoors/lake.py
@@ -1,5 +1,12 @@
-def draw_lake():
-    print("--------")
-    print("| lake |")
-    print("--------")
+def draw_lake(width = 1, length = 5):
+    for i in range(length):
+    	print('-', end = '')
+    print()
+    for i in range(width):
+    	for j in range(length):
+    		print('|', end = '')
+    	print()
+    for i in range(length):
+    	print('-', end = '')
+    print()
     return
